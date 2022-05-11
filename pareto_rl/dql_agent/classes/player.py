dc95c037750825896a771e34cf43d1d264fd27d5
Simone-Alghisi <alghisius.simone@gmail.com>
2022-05-11 14:17:35 +0200

    start remapping net actions to pairs

--- pareto_rl/dql_agent/classes/player.py
+++ pareto_rl/dql_agent/classes/player.py
@@ -1,171 +1,298 @@
 from poke_env.environment.double_battle import DoubleBattle
+from poke_env.environment.pokemon import Pokemon
 from poke_env.player.env_player import Gen8EnvSinglePlayer
 from gym.spaces import Space
-from poke_env.player.battle_order import BattleOrder, DoubleBattleOrder, ForfeitBattleOrder
+from poke_env.player.battle_order import (
+    BattleOrder,
+    DefaultBattleOrder,
+    DoubleBattleOrder,
+    ForfeitBattleOrder,
+)
 from poke_env.environment.battle import Battle
+from poke_env.environment.double_battle import DoubleBattle
+from pareto_rl.dql_agent.utils.move import Move
 from pareto_rl.dql_agent.utils.pokemon_mapper import PokemonMapper
+from typing import Union
+
 
 class SimpleRLPlayer(Gen8EnvSinglePlayer):
-  def __init__(self, **kwargs):
-    super(SimpleRLPlayer, self).__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super(SimpleRLPlayer, self).__init__(**kwargs)
 
-  def embed_battle(self, battle: DoubleBattle) -> list:
-    obs = []
-    active = []
-    bench = []
+    def embed_battle(self, battle: DoubleBattle) -> list:
+        obs = []
+        active = []
+        bench = []
 
-    for mon in battle.team.values():
-      # lots of info are available, the problem is time,
-      # hughes effect, and also mapping
-      mon_data = []
+        for mon in battle.team.values():
+            # lots of info are available, the problem is time,
+            # hughes effect, and also mapping
+            mon_data = []
 
-      # types (2)
-      types = [t.value if t is not None else -1 for t in mon.types]
-      mon_data.extend(types)
+            # types (2)
+            types = [t.value if t is not None else -1 for t in mon.types]
+            mon_data.extend(types)
 
-      # hp normalised (good idea?)
-      mon_data.append(mon.current_hp_fraction)
+            # hp normalised (good idea?)
+            mon_data.append(mon.current_hp_fraction)
 
-      # stats (5)
-      mon_data.extend(list(mon.stats.values()))
+            # stats (5)
+            mon_data.extend(list(mon.stats.values()))
 
-      # boosts and debuffs (7)
-      # TODO it may be possible to compute it together with
-      # the stats above to reduce the parameters
-      mon_data.extend(list(mon.boosts.values()))
+            # boosts and debuffs (7)
+            # TODO it may be possible to compute it together with
+            # the stats above to reduce the parameters
+            mon_data.extend(list(mon.boosts.values()))
 
-      # status
-      # TODO one-hot-encoding?
-      mon_data.append(mon.status.value if mon.status is not None else -1)
+            # status
+            # TODO one-hot-encoding?
+            mon_data.append(mon.status.value if mon.status is not None else -1)
 
-      # moves
-      # TODO... is it possible to have less than 4?
-      for move in mon.moves.values():
-        move_data = []
+            # moves
+            # TODO... is it possible to have less than 4?
+            for move in mon.moves.values():
+                move_data = []
 
-        # TODO... should we consider insering the move id?
-        # while it may be difficult to learn...
-        # it may be particularly useful to discriminate
-        # the final effect of the move if similar
-        # N.B this is a string, need to convert it using
-        # the MOVES dictionary from the json
-        # move_data.append(move._id)
+                # TODO... should we consider insering the move id?
+                # while it may be difficult to learn...
+                # it may be particularly useful to discriminate
+                # the final effect of the move if similar
+                # N.B this is a string, need to convert it using
+                # the MOVES dictionary from the json
+                # move_data.append(move._id)
 
-        # base power
-        move_damage = move.base_power
-        # consider STAB (same type attack bonus)
-        if move.type in mon.types:
-          move_damage *= 1.5
-        move_data.append(move_damage)
+                # base power
+                move_damage = move.base_power
+                # consider STAB (same type attack bonus)
+                if move.type in mon.types:
+                    move_damage *= 1.5
+                move_data.append(move_damage)
 
-        # priority
-        move_data.append(move.priority)
+                # priority
+                move_data.append(move.priority)
 
-        # accuracy
-        # TODO... should we encode together w/ damage?
-        move_data.append(move.accuracy)
+                # accuracy
+                # TODO... should we encode together w/ damage?
+                move_data.append(move.accuracy)
 
-        # category (?)
-        move_data.append(move.category.value)
+                # category (?)
+                move_data.append(move.category.value)
 
-        # pp (?)
-        # move_data.append(move.current_pp / move.max_pp)
+                # pp (?)
+                # move_data.append(move.current_pp / move.max_pp)
 
-        # recoil (?)
-        # move_data.append(move.recoil)
+                # recoil (?)
+                # move_data.append(move.recoil)
 
-        # damage for each active opponent (2)
-        for opp in battle.opponent_active_pokemon:
-          if opp is not None:
-            mlt = move.type.damage_multiplier(opp.type_1, opp.type_2)
-            move_data.append(move_damage*mlt)
-          else:
-            # if one is dead, append -1
-            move_data.append(-1)
+                # damage for each active opponent (2)
+                for opp in battle.opponent_active_pokemon:
+                    if opp is not None:
+                        mlt = move.type.damage_multiplier(opp.type_1, opp.type_2)
+                        move_data.append(move_damage * mlt)
+                    else:
+                        # if one is dead, append -1
+                        move_data.append(-1)
 
-        mon_data.extend(move_data)
+                mon_data.extend(move_data)
 
-      mon_data.extend([-1 for _ in range(6)]*(4-len(mon.moves)))
+            mon_data.extend([-1 for _ in range(6)] * (4 - len(mon.moves)))
 
-      if mon.active == True:
-        active.extend(mon_data)
-      else:
-        bench.extend(mon_data)
+            if mon.active == True:
+                active.extend(mon_data)
+            else:
+                bench.extend(mon_data)
 
-    obs.extend(active)
-    obs.extend(bench)
+        obs.extend(active)
+        obs.extend(bench)
 
-    # we could also take into account the opponents
-    # (which I would say is mandatory)
-    # and the field conditions (at least some of them)
-    return obs
+        # we could also take into account the opponents
+        # (which I would say is mandatory)
+        # and the field conditions (at least some of them)
+        return obs
+
+    def describe_embedding(self) -> Space:
+        return super().describe_embedding()
+
+    def calc_reward(self, last_battle, current_battle) -> float:
+        return self.reward_computing_helper(
+            last_battle, fainted_value=2, hp_value=1, victory_value=30
+        ) - self.reward_computing_helper(
+            current_battle, fainted_value=2, hp_value=1, victory_value=30
+        )
+        # return self.reward_computing_helper(current_battle,fainted_value=2,hp_value=1,victory_value=30)
+
+    def get_pokemon_order(self, action, idx, battle: DoubleBattle) -> BattleOrder:
+        poke_mapper = PokemonMapper(battle)
+        idx_to_pos = {0: -1, 1: -2}
+        pos = idx_to_pos[idx]
+        n_targets = 5
+        n_switches = 4
+        mon_actions = 4 * n_targets + n_switches
+        if pos in poke_mapper.moves_targets:
+            moves = [move for move in poke_mapper.moves_targets[pos].keys()]
+        if action < (mon_actions - n_switches) and not battle.force_switch[idx]:
+            # moves
+            move = action // n_targets
+            # target = DoubleBattle.OPPONENT_1_POSITION if action % 2 == 0 else DoubleBattle.OPPONENT_2_POSITION
+            target = (action % n_targets) - 2
+            if move >= len(moves):
+                import pdb
+
+                pdb.set_trace()
+            return self.agent.create_order(moves[move], move_target=target)
+        elif action >= (mon_actions - n_switches) and not battle.force_switch[idx]:
+            switch = action - (mon_actions - n_switches)
+            # print(idx, switch, battle.available_switches)
+            return self.agent.create_order(battle.available_switches[idx][switch])
+        else:
+            return self.agent.choose_random_move(battle)
+
+    def action_to_move(
+        self, actions, battle: DoubleBattle
+    ) -> BattleOrder:  # pyre-ignore
+        """Converts actions to move orders.
+        :param action: The action to convert.
+        :type action: int
+        :param battle: The battle in which to act.
+        :type battle: Battle
+        :return: the order to send to the server.
+        :rtype: str
+        """
+        actions = decode_actions(actions)
+        if actions[0] == -1 or actions[1] == -1:
+            return ForfeitBattleOrder()
+        battle_order = None
+        if battle.force_switch[0] or battle.force_switch[1]:
+            battle_order = DoubleBattleOrder(
+                self.agent.choose_random_move(battle), None
+            )
+        else:
+            first_order = None
+            second_order = None
+            for i, mon in enumerate(battle.active_pokemon):
+                if mon:
+                    if first_order is None:
+                        first_order = self.get_pokemon_order(actions[i], i, battle)
+                    else:
+                        second_order = self.get_pokemon_order(actions[i], i, battle)
+            battle_order = DoubleBattleOrder(first_order, second_order)
+
+        # print(battle_order)
+        return battle_order
 
 
-  def describe_embedding(self) -> Space:
-    return super().describe_embedding()
+# def decode_actions(coded_action):
+#    action1 = coded_action // 100
+#    action2 = coded_action % 100
+#    return [action1, action2]
 
-  def calc_reward(self, last_battle, current_battle) -> float:
-    return self.reward_computing_helper(last_battle,fainted_value=2,hp_value=1,victory_value=30)-self.reward_computing_helper(current_battle,fainted_value=2,hp_value=1,victory_value=30)
-    # return self.reward_computing_helper(current_battle,fainted_value=2,hp_value=1,victory_value=30)
 
-  def get_pokemon_order(self, action, idx, battle: Battle) -> BattleOrder:
-    poke_mapper = PokemonMapper(battle)
-    idx_to_pos = {0:-1, 1:-2}
-    pos = idx_to_pos[idx]
-    n_targets = 5
-    n_switches = 4
-    mon_actions = 4*n_targets + n_switches
-    if pos in poke_mapper.moves_targets:
-      moves = [ move for move in poke_mapper.moves_targets[pos].keys() ]
-    if(action < (mon_actions - n_switches) and not battle.force_switch[idx]):
-      #moves
-      move = action // n_targets
-      # target = DoubleBattle.OPPONENT_1_POSITION if action % 2 == 0 else DoubleBattle.OPPONENT_2_POSITION
-      target = (action % n_targets) - 2
-      if move >= len(moves):
-        import pdb; pdb.set_trace()
-      return self.agent.create_order(moves[move],move_target=target)
-    elif(action >= (mon_actions - n_switches) and not battle.force_switch[idx]):
-      switch = action - (mon_actions - n_switches)
-      # print(idx, switch, battle.available_switches)
-      return self.agent.create_order(battle.available_switches[idx][switch])
+def decode_action(
+    neuron_pos: int,
+    n_actions: int,
+    n_switches: int,
+    n_moves: int,
+    n_targets,
+    pm: PokemonMapper,
+) -> Union[DoubleBattleOrder, DefaultBattleOrder]:
+    if neuron_pos < n_actions * (n_moves * n_targets):
+        # the first one wants to make a move
+        move_1 = neuron_pos // n_actions  # [0, n_targets*n_moves)
+        move_1_idx = move_1 // n_targets  # [0, n_moves)
+        move_1_trg = move_1 % n_targets  # [0, n_targets)
+        act_2 = neuron_pos % n_actions  # [0, n_actions)
+        if act_2 < (n_moves * n_targets):  # [0, n_actions-n_switches), i.e. move
+            # the other wants to make a move
+            move_2_idx = act_2 // n_targets  # [0, n_moves)
+            move_2_trg = act_2 % n_targets  # [0, n_targets)
+            # search in available moves
+        else:
+            # the other wants to switch
+            switch_2_trg = act_2 - (n_moves * n_targets)  # [0, n_switches)
+            # search in available switches
+    elif neuron_pos < n_actions * (n_moves * n_targets) + (
+        (n_actions - 1) * n_switches
+    ):
+        switch = neuron_pos - (n_actions * (n_moves * n_targets))
+        switch_1_trg = switch // (n_actions - 1)  # [0, n_switches)
+        act_2 = switch % (n_actions - 1)  # [0, n_actions-1)
+        if act_2 < (n_moves * n_targets):  # i.e. a move
+            move_2_idx = act_2 // n_targets  # [0, n_moves)
+            move_2_trg = act_2 % n_targets  # [0, n_targets)
+            # search in available moves
+        else:
+            possible_switches = [
+                i for i in range(0, n_switches) if i != switch_1_trg
+            ]  # a list with only the possible target switches
+            switch_2_trg = possible_switches[
+                act_2 - (n_moves * n_targets)
+            ]  # [0, n_switches-1), but removing the same
+            # search in available switches
+    elif (
+        neuron_pos
+        < n_actions * (n_moves * n_targets)
+        + ((n_actions - 1) * n_switches)
+        + 2 * n_actions
+    ):
+        action = (
+            neuron_pos
+            - n_actions * (n_moves * n_targets)
+            + ((n_actions - 1) * n_switches)
+        )
+        act = action % n_actions
+        if action // n_actions == 0:
+            # first pokemon
+            if act < (n_moves * n_targets):
+                # the first pokemon wants to perform a move
+                move_1_idx = act // n_targets
+                move_1_trg = act % n_targets
+                # search in the first pokemon available moves
+            else:
+                # the first pokemon wants to perform a switch
+                switch_1_trg = act - (n_moves * n_targets)
+                # search in the first pokemon available switches
+        else:
+            # second pokemon
+            if act < (n_moves * n_targets):
+                # the second pokemon wants to perform a move
+                move_2_idx = act // n_targets
+                move_2_trg = act % n_targets
+                # search in the second pokemon available moves
+            else:
+                # the second pokemon wants to perform a switch
+                switch_2_trg = act - (n_moves * n_targets)
+                # search in the second pokemon available switches
     else:
-      return self.agent.choose_random_move(battle)
+        return DefaultBattleOrder()
 
 
-
-
-  def action_to_move(self, actions, battle: Battle) -> BattleOrder:  # pyre-ignore
-    """Converts actions to move orders.
-    :param action: The action to convert.
-    :type action: int
-    :param battle: The battle in which to act.
-    :type battle: Battle
-    :return: the order to send to the server.
-    :rtype: str
-    """
-    actions = decode_actions(actions)
-    if(actions[0] == -1 or actions[1] == -1):
-        return ForfeitBattleOrder()
-    battle_order = None
-    if battle.force_switch[0] or battle.force_switch[1]:
-      battle_order = DoubleBattleOrder(self.agent.choose_random_move(battle),None)
+def encode_action(
+    order: Union[BattleOrder, DoubleBattleOrder, DefaultBattleOrder],
+    n_actions: int,
+    n_switches: int,
+    n_moves: int,
+    n_targets,
+    pm: PokemonMapper,
+) -> int:
+    if isinstance(order, DefaultBattleOrder):
+        return (
+            n_actions * (n_moves * n_targets)
+            + (n_actions - 1) * n_switches
+            + 2 * n_actions
+            + 1
+        )
+    elif isinstance(order, BattleOrder):
+        idx = 0
+        # TODO we should check if the pokemon is the first or the second
     else:
-      first_order = None
-      second_order = None
-      for i, mon in enumerate(battle.active_pokemon):
-        if mon:
-          if first_order is None:
-            first_order = self.get_pokemon_order(actions[i],i,battle)
-          else:
-            second_order = self.get_pokemon_order(actions[i],i,battle)
-      battle_order = DoubleBattleOrder(first_order, second_order)
-
-    # print(battle_order)
-    return battle_order
-
-
-def decode_actions(coded_action):
-  action1 = coded_action//100
-  action2 = coded_action % 100
-  return [action1,action2]
+        first_order = order.first_order
+        second_order = order.second_order
+        # we need to handle the case in which we only have a single order
+        idx = 0
+        if isinstance(first_order, Move):
+            ...
+        elif isinstance(first_order, Pokemon):
+            ...
+        else:
+            print("i don't know what to do")
+            exit(1)
--- pareto_rl/dql_agent/utils/pokemon_mapper.py
+++ pareto_rl/dql_agent/utils/pokemon_mapper.py
@@ -1,4 +1,5 @@
 from poke_env.environment.double_battle import DoubleBattle
+from poke_env.player.battle_order import BattleOrder, DoubleBattleOrder
 from pareto_rl.dql_agent.utils.move import Move
 from poke_env.environment.pokemon import Pokemon
 from pareto_rl.dql_agent.utils.utils import get_possible_showdown_targets
@@ -25,14 +26,21 @@ class PokemonMapper:
         self.pos_to_mon: Dict[int, Pokemon] = {}
         self.mon_indexes: List[int] = []
         self.available_switches: Dict[int, list] = {}
+        self.available_orders: list[DoubleBattleOrder]
+        active_orders: List[List[BattleOrder]] = [[], []]
 
         # your mons
         pos = -1
-        for mon, moves, available_switches in zip(battle.active_pokemon, battle.available_moves, battle.available_switches):
+        for mon, moves, switches, orders in zip(
+            battle.active_pokemon,
+            battle.available_moves,
+            battle.available_switches,
+            active_orders,
+        ):
             if mon:
                 casted_moves: Set[Move] = {Move(move._id) for move in moves}
                 # map pokemons with their position
-                self.mapper(casted_moves, mon, pos, available_switches)
+                self.mapper(casted_moves, mon, pos, switches, orders)
             pos -= 1
 
         # opponent mons
@@ -40,16 +48,16 @@ class PokemonMapper:
         for mon in battle.opponent_active_pokemon:
             if mon:
                 # hardcoded opponent moves
-                moves: Set[Move]
+                opp_moves: Set[Move]
                 if mon.species.strip().lower() == "Zigzagoon".strip().lower():
-                    moves = {
+                    opp_moves = {
                         Move("doubleedge"),
                         Move("surf"),
                         Move("bodyslam"),
                         Move("thunderbolt"),
                     }
                 else:
-                    moves = {
+                    opp_moves = {
                         Move("energyball"),
                         Move("gigadrain"),
                         Move("knockoff"),
@@ -58,7 +66,7 @@ class PokemonMapper:
                 # TODO in the general case, first retrieve known moves and then infer
                 # the other probabilistically
                 # moves = mon.moves # pokemon used moves
-                self.mapper(moves, mon, pos)
+                self.mapper(opp_moves, mon, pos)
             pos += 1
 
         # remove invalid targets for basic
@@ -70,7 +78,16 @@ class PokemonMapper:
                         tmp_targets.remove(t)
                 self.moves_targets[pos][m] = tmp_targets
 
-    def mapper(self, moves: Set[Move], mon: Pokemon, pos: int, available_switches: Union[List[Pokemon],None] = None) -> None:
+        self.available_orders = DoubleBattleOrder.join_orders(*active_orders)
+
+    def mapper(
+        self,
+        moves: Set[Move],
+        mon: Pokemon,
+        pos: int,
+        available_switches: Union[List[Pokemon], None] = None,
+        orders: Union[List[BattleOrder], None] = None,
+    ) -> None:
         r"""
         Given a mon, its set of moves, and its position of the field, makes
         available additional information to the mapper, i.e.
@@ -85,14 +102,23 @@ class PokemonMapper:
             - pos: the position on the field of the mon
         """
         # get moves target
-        targets: Dict[Move, List[int]] = {
-            move: get_possible_showdown_targets(self.battle, mon, move, pos)
-            for move in moves
-        }
-        original_targets: Dict[Move, List[int]] = {
-            move: get_possible_showdown_targets(self.battle, mon, move, pos, pareto=False)
-            for move in moves
-        }
+        targets: Dict[Move, List[int]] = {}
+        original_targets: Dict[Move, List[int]] = {}
+        for move in moves:
+            ot, pt = get_possible_showdown_targets(self.battle, mon, move, pos)
+            targets[move] = pt
+            original_targets[move] = ot
+            if orders is not None:
+                orders.extend(
+                    [
+                        BattleOrder(move, move_target=target)
+                        for target in original_targets[move]
+                    ]
+                )
+
+        if orders is not None and available_switches is not None:
+            orders.extend([BattleOrder(switch) for switch in available_switches])
+
         # map the target with its position
         self.moves_targets[pos] = targets
         self.original_moves_targets[pos] = original_targets
@@ -101,7 +127,7 @@ class PokemonMapper:
         self.mon_indexes.append(pos)
 
         if available_switches is not None:
-          self.available_switches[pos] = available_switches
+            self.available_switches[pos] = available_switches
 
     def alive_pokemon_number(self) -> int:
         r"""
@@ -122,3 +148,7 @@ class PokemonMapper:
             pos: the position of the mon associated to index
         """
         return self.mon_indexes[index // 2]
+
+    def mask_unavailable_moves(self):
+        # TODO, remap moves
+        ...
