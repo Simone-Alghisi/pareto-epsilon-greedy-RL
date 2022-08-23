128c8d866f478b21025828278f7bcf4cdde0e63d
samuelebortolotti <samuele.bortolotti@hotmail.it>
2022-08-23 14:32:55 +0200

    fix requirements and little changes to smoothing lines

--- analysis/10_runs_agg.r
+++ analysis/10_runs_agg.r
@@ -6,17 +6,16 @@ runs.pareto <- read.csv("./data/pareto_run.csv")
 runs.pareto <- runs.pareto %>% select(contains("ep_reward") & -contains("MIN") & -contains("MAX") | contains("episode"))
 runs.random <- runs.random %>% select(contains("ep_reward") & -contains("MIN") & -contains("MAX"))
 
-runs.pareto <- runs.pareto %>% mutate( mean_pareto = rowMeans(select(.,-contains("episode"))))
-runs.random <- runs.random %>% mutate( mean_random = rowMeans(select(.,-contains("episode"))))
-
+runs.pareto <- runs.pareto %>% mutate(mean_pareto = rowMeans(select(.,-contains("episode"))))
+runs.random <- runs.random %>% mutate(mean_random = rowMeans(select(.,-contains("episode"))))
 
 export <- bind_cols(runs.random,runs.pareto)
 
 ggplot(data=export,aes(x=episode)) +
   geom_line(aes(y=mean_pareto,color="pareto"),alpha=0.2) + 
   geom_line(aes(y=mean_random,colour="random"),alpha=0.2) + 
-  geom_smooth(aes(y=mean_pareto,colour="pareto"),method = "loess",span=0.2) +
-  geom_smooth(aes(y=mean_random,colour="random"),method = "loess",span=0.2) + 
+  geom_smooth(aes(y=mean_pareto,colour="pareto"),method = "loess",span=0.2,formula=y ~ x) +
+  geom_smooth(aes(y=mean_random,colour="random"),method = "loess",span=0.2,formula=y ~ x) + 
   theme_minimal() + 
   labs(x="Episode",y="Episode Reward") +
   scale_color_manual(name = c("Pareto", "Random"), 
--- requirements.R
+++ requirements.R
@@ -1,36 +1,36 @@
-##
+# #
 # Function which loads the packages passed as input
-# Adapted from: https://stackoverflow.com/questions/38928326/is-there-something-like-requirements-txt-for-r
+# Adapted from: https: //stackoverflow.com/questions/38928326/is-there-something-like-requirements-txt-for-r
 #
 # Args:
-# - packages [str]: packages list
+# - packages[str]: packages list
 #
 installPackages <- function(packages = "default") {
-	# install default packages if required
-	if(length(packages) == 1L && packages == "default") {
-		packages <- c(
-			"plyr", 
-			"dplyr", 
+  # install default packages if required
+  if (length(packages) == 1L && packages == "default") {
+    packages <- c(
+      "plyr",
+      "dplyr",
       "tibble",
-			"devtools",
-			"ggplot2", 
-			"utils",
-			"ggpmisc",
-			"optparse"
-		)
-	}
-	packagecheck <- match(packages, utils::installed.packages()[,1])
-	# packages to install
-	packagestoinstall <- packages[ is.na( packagecheck ) ]
-	if(length( packagestoinstall ) > 0L) {
-		print("Installing the requested packages...")
-		utils::install.packages(
-			packagestoinstall, 
-			repos = "https://cran.mirror.garr.it/CRAN/"
-		)
-	}else{
-		print("All requested packages are already installed")
-	}
+      "devtools",
+      "ggplot2",
+      "utils",
+      "ggpmisc",
+      "optparse"
+    )
+  }
+  packagecheck <- match(packages, utils::installed.packages()[, 1])
+  # packages to install
+  packagestoinstall <- packages[is.na(packagecheck)]
+  if (length(packagestoinstall) > 0L) {
+    print("Installing the requested packages...")
+    utils::install.packages(
+      packagestoinstall,
+      repos = "https://cran.mirror.garr.it/CRAN/"
+    )
+  } else {
+    print("All requested packages are already installed")
+  }
 }
 
 # Install packages
